e9d9b2ca13b1c269d193abb5fa2364fde20425af
Dhruv sharma <dhsharma27@gmail.com>
2016-12-25 17:03:01 +0530

    calendar thing working

--- Calendar.rb
+++ Calendar.rb
@@ -1,49 +1,44 @@
 module Calendar
-  require 'Nokigiri'
+  require 'Nokogiri'
   require 'open-uri'
   require 'set'
   require 'Date'
+  require 'Pry'
 
   URL_TEMP = 'https://www.timeanddate.com/holidays/india/'
-  CHROMOSOME = ''
-  DD = nil
 
-  def set_date
-    DD = Date.today
+  CHROMOSOME = ''
+  DD = Date.today
+
+  def Calendar.valid_holiday(d)
+    d['class']!='head' && ( d.css('td').last.text=='Gazetted Holiday') #'Restricted Holiday'
   end
 
-  def get_holidays
-    set_date
+
+  def Calendar.get_holidays
     yy =  DD.year
     holidays = Set.new
     urls = [  URL_TEMP+yy.to_s , URL_TEMP+(yy+1).to_s  ]
     
     urls.each_with_index do |i,index|
-      table = Nokogiri::HTML(open(i)).page.css('table')
-
-      table.each do |j|
-        begin
-          holidays.add( Date.parse(j.css('th')+" "+(yy+index).to_s) )
-        rescue ArgumentError
-        end
-      end        
+      table = Nokogiri::HTML(open(i)).css('table')
+      table.search('tr').each do |j|  
+        holidays.add( Date.parse(j.css('th').text+" "+(yy+index).to_s) ) if Calendar::valid_holiday(j)
+      end
+    end        
     
-    end
-
     holidays
-
   end
 
-  def mapping(date,holidays)
-    return 1 if date.wday==0 || date.wday==6    #sat/sun fixed holidays
-    return 2 if holidays.include? date
-    return 0
+  def Calendar.mapping(date,holidays)
+    return '1' if date.wday==0 || date.wday==6  || holidays.include?(date)    #sat/sun/gazetted holidays
+    return '0'
   end
 
-  def make_chromosomes
-    holidays = get_holidays
+  def Calendar.make_chromosomes
+    holidays = Calendar::get_holidays
 
-    (DD..DD+366).each do |i|
+    (DD..DD+365).each do |i|
       CHROMOSOME<<mapping(i,holidays)
     end
 
