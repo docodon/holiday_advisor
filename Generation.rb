0706b129f2f959b7f0951521578f31c7f37d6f4a
Dhruv sharma <dhsharma27@gmail.com>
2016-12-25 21:08:01 +0530

    added class for generation

---
+++ Generation.rb
@@ -0,0 +1,13 @@
+require './Chromosome.rb'
+
+class Generation
+  
+  def initialize pop_size,leaves
+    @generation = [Chromosome.new(leaves)]*pop_size
+  end
+
+  def get
+    @generation
+  end
+
+end
